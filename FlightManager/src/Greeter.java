f813cef8c888b201b93df67bcb5fc482fe818dc6
Eric Lindroth <ericl87@hotmail.com>
2013-04-22 11:18:13 +0200

    TestCommit
    HelloWorld

---
+++ FlightManager/src/Greeter.java
@@ -0,0 +1,9 @@
+
+public class Greeter {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!\n");
+		System.out.print("This is a HelloWorld script; a common first step with any programming language.");
+	}
+
+}
